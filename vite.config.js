c80a965fc76d2cb9086b8bfc5e52552870aa859b
LGoulart <leandrovgoulart@gmail.com>
2023-10-02 10:05:47 -0300

    config deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/magazineIntensivao/",
+  base: "https://github.com/LeandroGoulart/e-commerce/",
   plugins: [],
   resolve: {
     /*something*/
